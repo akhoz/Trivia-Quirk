4a96aa12ce63051e9e5c4b02c01a0bee6de764d1
iZackk26 <newxstorecr@gmail.com>
2023-11-09 23:02:56 -0600

    Adding first base code for the Home-GUI

--- src/main/java/GUI/Home.java
+++ src/main/java/GUI/Home.java
@@ -1,6 +1,41 @@
 package GUI;
 import javax.swing.*;
+import java.awt.*;
 
-public class Home {
-    
+public class Home extends JFrame {
+    public Home () {
+        JPanel panel = new JPanel(new BorderLayout());
+        JPanel panelBotones = new JPanel(new FlowLayout());
+        JLabel titulo = new JLabel("Trivia Quirk");
+        Font fuente = new Font("Arial", Font.BOLD, 30);
+
+        Dimension dim = new Dimension(250, 50);
+        JButton singlePlayer = new JButton("Un jugador");
+        JButton multiPlayer = new JButton("Multijugador");
+        JButton exit = new JButton("Salir");
+        singlePlayer.setPreferredSize(dim);
+        multiPlayer.setPreferredSize(dim);
+        exit.setPreferredSize(dim);
+
+        panelBotones.add(singlePlayer, BorderLayout.CENTER);
+        panelBotones.add(multiPlayer, BorderLayout.CENTER);
+
+
+
+        titulo.setFont(fuente);
+
+        titulo.setHorizontalAlignment(JLabel.CENTER);
+
+        panel.add(titulo, BorderLayout.NORTH);
+        panel.add(panelBotones, BorderLayout.CENTER);
+        add(exit, BorderLayout.SOUTH);
+        add(panel);
+
+        setTitle("Trivia Quirk");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
+import GUI.Home;
 import poo.proyecto2.triviaquirk.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new Home();
     }
 }
