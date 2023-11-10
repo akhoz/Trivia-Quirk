1e22231506b2ba2b307dc86de3b1a29e0fa8ec0c
akhoz <adrian25092005@gmail.com>
2023-11-09 21:30:35 -0600

    First commit

---
+++ src/Categorias/Categoria.java
@@ -0,0 +1,13 @@
+package Categorias;
+
+import poo.proyecto2.triviaquirk.iCategorias;
+import poo.proyecto2.triviaquirk.excepciones.excepcionPartidaNoDisponible;
+import poo.proyecto2.triviaquirk.excepciones.excepcionPreguntasNoDisponibles;
+
+import java.util.ArrayList;
+public class Categoria implements iCategorias{
+
+    protected String nombre;
+
+
+}
