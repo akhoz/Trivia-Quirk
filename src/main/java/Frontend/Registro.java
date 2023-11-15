ef86064d9033b150286a058941751c565d3c9186
akhoz <adrian25092005@gmail.com>
2023-11-15 15:06:10 -0600

    Implementing different function to category registration

--- src/main/java/Frontend/Registro.java
+++ src/main/java/Frontend/Registro.java
@@ -13,6 +13,7 @@ public class Registro extends JFrame {
 
     private int cantidad;
     private int numeroDePartida;
+    private int cantidadDeCategoriasaJugar;
     private ArrayList<String> jugadoresRegistrados = new ArrayList<>();
     private Partida partida;
     private Jugador jugadores;
@@ -21,7 +22,7 @@ public class Registro extends JFrame {
     public Registro() {
         //Ventana------------------------------------------------------------------------------------------------------
         JFrame ventana = new JFrame("Registrando jugadores");
-        ventana.setSize(600, 300);
+        ventana.setSize(600, 400);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Fuentes-------------------------------------------------------------------------------------------------------
@@ -32,16 +33,20 @@ public class Registro extends JFrame {
         JLabel cantidadJugadoresLabel = new JLabel();
         JLabel nombreJugadorLabel = new JLabel();
         JLabel jugadoresRestantes = new JLabel();
+        JLabel cantidadCategoriasLabel = new JLabel();
         JLabel categoriasDisponibles = new JLabel();
         JLabel categoriaLabel = new JLabel();
+        JLabel categoriasRestantes = new JLabel();
 
         JTextField cantidadJugadores = new JTextField();
         JTextField nombreJugador = new JTextField();
+        JTextField cantidadCategorias = new JTextField();
         JTextField categoriaTextField = new JTextField();
 
         JButton confirmar = new JButton("Confirmar");
         JButton registrar = new JButton("Registrar");
-        JButton jugar = new JButton("Jugar");
+        JButton confirmarCategorias = new JButton("Confirmar");
+        JButton agregar = new JButton("Agregar");
         JButton regresar = new JButton("Regresar");
 
         //Implementacion------------------------------------------------------------------------------------------------
@@ -58,7 +63,7 @@ public class Registro extends JFrame {
         confirmar.setBounds(300, 10, 100, 30);
         ventana.add(confirmar);
 
-        regresar.setBounds(5, 227, 100, 30);
+        regresar.setBounds(5, 327, 100, 30);
         ventana.add(regresar);
 
         nombreJugadorLabel.setVisible(false);
@@ -88,22 +93,42 @@ public class Registro extends JFrame {
         categoriasDisponibles.setText("Categorias disponibles: Futbol, Basket, Matematica, Videojuegos");
         ventana.add(categoriasDisponibles);
 
+        cantidadCategoriasLabel.setVisible(false);
+        cantidadCategoriasLabel.setBounds(10, 170, 200, 30);
+        cantidadCategoriasLabel.setFont(fuente);
+        cantidadCategoriasLabel.setText("Cantidad de categorías: ");
+        ventana.add(cantidadCategoriasLabel);
+
+        cantidadCategorias.setVisible(false);
+        cantidadCategorias.setBounds(190, 170, 100, 30);
+        cantidadCategorias.setFont(fuenteSecundaria);
+        setPlaceholder(cantidadCategorias, "Cantidad de categorías");
+        ventana.add(cantidadCategorias);
+
+        confirmarCategorias.setVisible(false);
+        confirmarCategorias.setBounds(300, 170, 100, 30);
+        ventana.add(confirmarCategorias);
+
         categoriaLabel.setVisible(false);
-        categoriaLabel.setBounds(10, 170, 200, 30);
+        categoriaLabel.setBounds(10, 210, 200, 30);
         categoriaLabel.setFont(fuente);
         categoriaLabel.setText("Elección de categoria: ");
         ventana.add(categoriaLabel);
 
-        categoriaTextField .setVisible(false);
-        categoriaTextField .setBounds(190, 170, 100, 30);
-        categoriaTextField .setFont(fuenteSecundaria);
+        categoriaTextField.setVisible(false);
+        categoriaTextField.setBounds(190, 210, 100, 30);
+        categoriaTextField.setFont(fuenteSecundaria);
         setPlaceholder(categoriaTextField , "Categoria");
         ventana.add(categoriaTextField );
 
-        jugar.setVisible(false);
-        jugar.setBounds(300, 170, 100, 30);
-        ventana.add(jugar);
+        agregar.setVisible(false);
+        agregar.setBounds(300, 210, 100, 30);
+        ventana.add(agregar);
 
+        categoriasRestantes.setVisible(false);
+        categoriasRestantes.setBounds(10, 250, 600, 30);
+        categoriasRestantes.setFont(fuente);
+        ventana.add(categoriasRestantes);
         //Acciones------------------------------------------------------------------------------------------------------
         confirmar.addActionListener(e -> {
             String input = cantidadJugadores.getText();
@@ -157,9 +182,9 @@ public class Registro extends JFrame {
                         registrar.setEnabled(false);
 
                         categoriasDisponibles.setVisible(true);
-                        categoriaLabel.setVisible(true);
-                        categoriaTextField .setVisible(true);
-                        jugar.setVisible(true);
+                        cantidadCategoriasLabel.setVisible(true);
+                        cantidadCategorias.setVisible(true);
+                        confirmarCategorias.setVisible(true);
 
                         numeroDePartida = categoria.registrarPartida();
                         System.out.println("Numero de partida: " + numeroDePartida);
@@ -177,17 +202,48 @@ public class Registro extends JFrame {
             }
         });
 
-        jugar.addActionListener(e -> {
-            String input = categoriaTextField.getText().toLowerCase();
+        confirmarCategorias.addActionListener(e -> {
+            String input = cantidadCategorias.getText();
 
-            if (input.equals("categoria") || input.equals("")) {
-                JOptionPane.showMessageDialog(null, "Ingrese la categoria");
-            } else if (!input.equals("futbol") && !input.equals("basket") && !input.equals("matematica") && !input.equals("videojuegos")){
-                JOptionPane.showMessageDialog(null, "Ingrese una categoria válida");
+            try {
+                cantidadDeCategoriasaJugar = Integer.parseInt(input);
+                System.out.println("Cantidad de categorias: " + cantidadDeCategoriasaJugar);
+
+                if (cantidadDeCategoriasaJugar > 0) {
+                    categoriaLabel.setVisible(true);
+                    categoriaTextField.setVisible(true);
+                    agregar.setVisible(true);
+                    categoriasRestantes.setVisible(true);
+                    categoriasRestantes.setText("Categorias restantes: " + cantidadDeCategoriasaJugar);
+                    confirmarCategorias.setEnabled(false);
+                    cantidadCategorias.setFocusable(false);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Ingrese un número mayor a 0");
+                }
+            }  catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Ingrese un valor numérico válido");
+            }
+        });
+
+        agregar.addActionListener(e -> {
+
+            if (cantidadDeCategoriasaJugar > 1) {
+
+                String input = categoriaTextField.getText().toLowerCase();
+
+                if (input.equals("categoria") || input.equals("")) {
+                    JOptionPane.showMessageDialog(null, "Ingrese la categoria");
+                } else if (!input.equals("futbol") && !input.equals("basket") && !input.equals("matematica") && !input.equals("videojuegos")) {
+                    JOptionPane.showMessageDialog(null, "Ingrese una categoria válida");
+                } else {
+                    setPlaceholder(categoriaTextField, "Categoria");
+                    System.out.println("Categoria seleccionada: " +  input);
+                    categoria.appendCategoria(input);
+                    --cantidadDeCategoriasaJugar;
+                    categoriasRestantes.setText("Categorias restantes: " + cantidadDeCategoriasaJugar);
+                }
             } else {
-                setPlaceholder(categoriaTextField , "Categoria");
-                categoria.setCategoria(input);
-                System.out.println("Categoria seleccionada: " + categoria.nombreCategoria());
+                categoria.setCategoria(categoria.getCategorias().get(0));
                 new Trivia(categoria, partida);
                 ventana.dispose();
             }
